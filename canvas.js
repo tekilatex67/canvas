d84b096d993673fd1b9085930465a9559e1cfef8
Klipfel Nicolas <nos_67210@hotmail.fr>
2020-04-24 13:29:52 +0200

    ajout couleur et width canvas style

--- canvas.js
+++ canvas.js
@@ -11,18 +11,37 @@ ctx.lineWidth = 10;
 
 let positionX = 0;
 let positionY = 0;
+let hue = 0;
 let isDawn = false;
+let direction = true;
 
 function draw(e) {
 
     if (!isDawn) return;
 
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     ctx.moveTo(positionX, positionY);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 
     [positionX, positionY] = [e.offsetX, e.offsetY];
+    hue++;
+
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+
+    if (ctx.lineWidth >= 50 || ctx.lineWidth <= 10) {
+        direction = !direction;
+    }
+
 }
 
 canvas.addEventListener('mousemove', draw);
